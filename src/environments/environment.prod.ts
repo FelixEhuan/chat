c1031a5ca28b0767c74db980e452eb955bb52d58
Soft-listig <soft.listig@gmail.com>
2018-05-21 16:30:02 -0500

    Commit 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBkF7tJTYANGEeh6RrE6esYobL5YBlNSwA",
+    authDomain: "firechat-5e2e0.firebaseapp.com",
+    databaseURL: "https://firechat-5e2e0.firebaseio.com",
+    projectId: "firechat-5e2e0",
+    storageBucket: "firechat-5e2e0.appspot.com",
+    messagingSenderId: "592372287717"
+}
 };
